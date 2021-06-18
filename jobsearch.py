5ae8d6e807281a1a46aec56ef25e592137899971
micko45 <michaelmcgearty@gmail.com>
2021-06-18 14:53:16 +0100

    here we go again

--- jobsearch.py
+++ jobsearch.py
@@ -7,12 +7,11 @@ import sqlite3
 from db import get_last_date
 import re
 
-
-pk_file = "./files/pikle.pk"
+pk_file = "./files/pikle.pk" #Pickle file to mail later
 pd.set_option('display.max_colwidth', -1) #Pandas tuncates on raspberry pi. 
-db = './files/db.sql'
+db = './files/db.sql' #We always need a DB
 
-#some job sites are iffy when it comes to headers
+#some job sites are iffy when it comes to headers so spoof chrome
 headers = {
 "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9", 
 "Accept-Encoding": "gzip, deflate", 
@@ -26,19 +25,19 @@ headers = {
 url = 'https://www.jobs.ie/linux-jobs'
 r = requests.get(url, headers = headers)
 soup = bs(r.content, 'html.parser')
-data = soup.find_all('div', {'class':"job-details-header serp-item default"})
+data = soup.find_all('div', {'class':"job-details-header serp-item default"}) # Pull down only the job sections
 
 #Set up irishjobs.ie
 url2 = "https://www.irishjobs.ie/ShowResults.aspx?Keywords=linux&autosuggestEndpoint=%2Fautosuggest&Location=0&Category=&Recruiter=Company&Recruiter=Agency&btnSubmit=Search"
 r2 = requests.get(url2, headers = headers)
 soup2 = bs(r2.content, 'html.parser')
-# data2 = soup2.find_all('div', {'class':'module job-result'})
-data2 = soup2.find_all('div', {'class':  re.compile(r'module job-result')})
+# data2 = soup2.find_all('div', {'class':'module job-result'}) #Weirdly wont work on python 3.5
+data2 = soup2.find_all('div', {'class':  re.compile(r'module job-result')}) #Works on python 3.5 but seems slower
 
 year = datetime.datetime.now().strftime("%Y")
 
 def day_date(x = "1 Day ago"):
-    #Ago is not a date
+    #Ago is not a date so make it one
     s = x.lower().replace('day ', 'days ')
     parsed_s = [s.split()[:2]]
     time_dict = dict((fmt,float(amount)) for amount,fmt in parsed_s)
@@ -93,28 +92,32 @@ def irishjobs(a):
         jobID = url.split('-')[-1].split('.')[0]
         lastDate = get_last_date(jobID)
         a.append([title, url, location, comp, updated, site, jobID, lastDate])
-        #a.append([title, url, location, comp, updated, site, jobID])
 
 def mk_df(a):
     #make a dataframe from all the shit got from irishjobs.ie and jobs.ie
+    #lastDate sucks and should be name changned as its first date job appeared
     df = pd.DataFrame(a, columns = ['title', 'url', 'location', 'comp', 'updated', 'site', 'JobID', 'lastDate'])
     
     for index, row in df.iterrows():
+        #Dates are messy
         if "Today" in df.loc[index, 'updated']:
+	    #Change it from "today" to the date
             df.loc[index, 'updated'] = today()
 
         elif "Day" in df.loc[index, 'updated']:
-                df.loc[index, 'updated'] = day_date(df.loc[index, 'updated'])
+            #some of them say "update 1 Day ago" which is not good for my code
+            df.loc[index, 'updated'] = day_date(df.loc[index, 'updated'])
 
         elif "/" not in df.loc[index, 'updated']:
+	    #Some times the date just has a month like "1 of June"
             df.loc[index, 'updated'] = tidy_date(df.loc[index, 'updated'] + " " + year)
 
         #Clean up company stuff. 
         df.loc[index, 'comp'] = convert_comp( df.loc[index, 'comp'])
     
-    df['updated'] =pd.to_datetime(df.updated, dayfirst=True).dt.date #make the date a date 
+    df['updated'] =pd.to_datetime(df.updated, dayfirst=True).dt.date #make the date a date type
     df = df.sort_values(by = 'updated', ascending=False) #sort by date
-    df['url'] = '<a href=' + df['url'] + '><div>' + 'url' + '</div></a>' # make it a url anchor
+    df['url'] = '<a href=' + df['url'] + '><div>' + 'url' + '</div></a>' # make the url a anchor
     
     return df
 
@@ -124,11 +127,15 @@ def main():
   jobsie(a)
   df = mk_df(a)
 
+  #dump to Database stuff, should be in its own function but i am tired. 
+  #Also should add a job to clean up old db stuff to keep file small. 
   cnx = sqlite3.connect(db)
   df_2_dump = df.drop('lastDate', axis=1)
   df_2_dump.to_sql(name='jobs', con=cnx, if_exists='append')
-
+  
+  #Dump to pickle file so we can mail it later
   pickle.dump(df.to_html(escape = False), open(pk_file, 'wb'))
+
   print(df.head())
  
 main()
