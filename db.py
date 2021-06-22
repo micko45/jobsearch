0da9e589d8e05f3d030c824b9575ec2992e950d2
micko45 <michaelmcgearty@gmail.com>
2021-06-22 15:27:36 +0100

    here we go again

--- db.py
+++ db.py
@@ -1,5 +1,5 @@
 import sqlite3
-from datetime import datetime
+from datetime import datetime, timedelta, date
 
 db = './files/db.sql'
 con = sqlite3.connect(db)
@@ -7,21 +7,46 @@ cur = con.cursor()
 
 #select updated from jobs where JobID = 8620334 order by date(updated) ASC limit 1;
 
-def get_last_date(jobid):
+def todays_date():
+  return date.today()
+
+def yesterdays_date():
+  today = todays_date()
+  return today - timedelta(days = 1)
+
+def get_last_date(jobid, today = str(todays_date()), yesterday = yesterdays_date()):
  
   sql = "SELECT updated FROM jobs WHERE JobID = {} ORDER BY DATE(updated) ASC LIMIT 1".format(int(jobid))
   cur.execute(sql)
-  data = cur.fetchall()
-  for d in data:
-     return  "".join(d)
+  sql_data = cur.fetchall()
+  for d in sql_data:
+     data =  "".join(d)
+
+     if data == today:
+       return "Today"
+
+     elif data == yesterday:
+       return "Yesterday"
+
+     else: return data
 
 def get_oldest_date(jobid):
+  today = str(todays_date())
+  yesterday = str(yesterdays_date())
  
   sql = "SELECT updated FROM _jobs WHERE jobid = {} ORDER BY DATE(updated) ASC LIMIT 1".format(int(jobid))
   cur.execute(sql)
   data = cur.fetchall()
   for d in data:
-     return  "".join(d)
+     data =  "".join(d)
+
+     if data == today:
+       return "Today"
+
+     elif data == yesterday:
+       return "Yesterday"
+
+     else: return data
 
 def df_2_db(df):
 
@@ -34,11 +59,19 @@ def df_2_db(df):
  
 
 if __name__ == "__main__":
-  df_2_db()
+#  df_2_db()
 #  print(get_last_date(8620558))
-#  for i in [ 8620558, 1954113, 1956471 ]:
-#    d2 = get_oldest_date(i)
-#    today = datetime.now().strftime("%Y-%m-%d")
-#    if today == d2:
-#      print(today, " and ", d2 , "match", i )
-#    else: print(today, d2, i)
+  print(yesterdays_date())
+  for i in [ 8620558, 1954113, 1956471, 8627436, 1954237, 8625984 ]:
+    d2 = get_oldest_date(i)
+    print(d2)
+   # today = str(todays_date())
+   # yesterday = str(yesterdays_date())
+    #today = datetime.now().strftime("%Y-%m-%d")
+   # if today == d2:
+   #   #print(today, " and ", d2 , "match", i )
+   #   print("today is ", d2)
+   # elif d2 == yesterday:
+   #   print("yesterday was ", d2)
+   # else: print(today, d2, i)
+#  print("yesterday " , today - timedelta(days = 1))
